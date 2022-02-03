5a095505a5619063b79d97aac3bc298e09c31e3f
Karol  Soares <karolina.soares@sumup.com>
2022-02-02 22:02:44 -0300

    Atualizando Hello World rodando

---
+++ helloworldnew/src/main/java/br/org/generation/helloworldnew/HelloworldnewApplication.java
@@ -0,0 +1,13 @@
+package br.org.generation.helloworldnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloworldnewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloworldnewApplication.class, args);
+	}
+
+}
---
+++ helloworldnew/src/test/java/br/org/generation/helloworldnew/HelloworldnewApplicationTests.java
@@ -0,0 +1,13 @@
+package br.org.generation.helloworldnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldnewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
